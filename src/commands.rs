3c9b2017894193e225d88c226a067eb8e3c56444
Aaron Riekenberg <aaron.riekenberg@target.com>
2022-11-18 16:07:35 -0600

    Add command service.

--- src/commands.rs
+++ src/commands.rs
@@ -1,5 +1,6 @@
+mod service;
+
 use axum::{
-    async_trait,
     extract::{Extension, Path},
     http::StatusCode,
     response::{IntoResponse, Response},
@@ -7,72 +8,14 @@ use axum::{
     Json, Router,
 };
 
-use chrono::prelude::{Local, SecondsFormat};
-
-use serde::Serialize;
-
-use std::{collections::HashMap, sync::Arc, time::Instant};
-
-use tokio::process::Command;
-
-fn current_time_string() -> String {
-    Local::now().to_rfc3339_opts(SecondsFormat::Nanos, true)
-}
-
-#[derive(Debug, Serialize)]
-struct RunCommandResponse {
-    now: String,
-    command_duration_ms: u128,
-    command_info: &'static crate::config::CommandInfo,
-    command_output: String,
-}
-
-#[derive(Debug)]
-
-enum RunCommandError {
-    CommandNotFound,
-}
+use service::{DynCommandsService, RunCommandError, RunCommandResponse};
 
 impl IntoResponse for RunCommandError {
     fn into_response(self) -> Response {
         match self {
             Self::CommandNotFound => StatusCode::NOT_FOUND.into_response(),
+            Self::SemaphoreAcquireError => StatusCode::TOO_MANY_REQUESTS.into_response(),
         }
-    }
-}
-
-#[async_trait]
-trait CommandsService {
-    async fn run_command(&self, id: &str) -> Result<RunCommandResponse, RunCommandError>;
-}
-
-type DynCommandsService = Arc<dyn CommandsService + Send + Sync>;
-
-struct CommandsServiceImpl {
-    id_to_command_info: HashMap<String, &'static crate::config::CommandInfo>,
-}
-
-impl CommandsServiceImpl {
-    fn new() -> Arc<Self> {
-        Arc::new(Self {
-            id_to_command_info: crate::config::instance()
-                .command_configuration()
-                .commands()
-                .iter()
-                .map(|command_config| (command_config.id().clone(), command_config))
-                .collect(),
-        })
-    }
-}
-
-#[async_trait]
-impl CommandsService for CommandsServiceImpl {
-    async fn run_command(&self, id: &str) -> Result<RunCommandResponse, RunCommandError> {
-        let command_info = self
-            .id_to_command_info
-            .get(id)
-            .cloned()
-            .ok_or(RunCommandError::CommandNotFound)?;
     }
 }
 
@@ -84,7 +27,7 @@ async fn run_command(
     Path(id): Path<String>,
     Extension(commands_service): Extension<DynCommandsService>,
 ) -> Result<Json<RunCommandResponse>, RunCommandError> {
-    tracing::info!("in run_command id = {}", id);
+    tracing::debug!("in run_command id = {}", id);
 
     let response = commands_service.run_command(&id).await?;
 
@@ -92,7 +35,7 @@ async fn run_command(
 }
 
 pub fn router() -> Router {
-    let commands_service: DynCommandsService = CommandsServiceImpl::new();
+    let commands_service: DynCommandsService = service::new_commands_service();
 
     Router::new()
         .route("/commands", get(get_all_commands))
---
+++ src/commands/service.rs
@@ -0,0 +1,114 @@
+use axum::async_trait;
+
+use chrono::prelude::{Local, SecondsFormat};
+
+use serde::Serialize;
+
+use std::{collections::HashMap, sync::Arc};
+
+use tokio::{
+    process::Command,
+    sync::{Semaphore, SemaphorePermit},
+    time::{Duration, Instant},
+};
+
+#[async_trait]
+pub trait CommandsService {
+    async fn run_command(&self, command_id: &str) -> Result<RunCommandResponse, RunCommandError>;
+}
+
+pub type DynCommandsService = Arc<dyn CommandsService + Send + Sync>;
+
+#[derive(Debug, Serialize)]
+pub struct RunCommandResponse {
+    now: String,
+    command_duration_ms: u128,
+    command_info: &'static crate::config::CommandInfo,
+    command_output: String,
+}
+
+#[derive(Debug)]
+pub enum RunCommandError {
+    CommandNotFound,
+    SemaphoreAcquireError,
+}
+
+pub fn new_commands_service() -> DynCommandsService {
+    CommandsServiceImpl::new()
+}
+
+struct CommandsServiceImpl {
+    id_to_command_info: HashMap<String, &'static crate::config::CommandInfo>,
+    semapore: Semaphore,
+    semapore_acquire_timeout: Duration,
+}
+
+impl CommandsServiceImpl {
+    fn new() -> Arc<Self> {
+        let command_configuration = crate::config::instance().command_configuration();
+
+        Arc::new(Self {
+            id_to_command_info: command_configuration
+                .commands()
+                .iter()
+                .map(|command_config| (command_config.id().clone(), command_config))
+                .collect(),
+            semapore: Semaphore::new(*command_configuration.max_concurrent_commands()),
+            semapore_acquire_timeout: *command_configuration.semaphore_acquire_timeout(),
+        })
+    }
+
+    async fn acquire_semaphore(&self) -> Result<SemaphorePermit<'_>, RunCommandError> {
+        let result = tokio::time::timeout(self.semapore_acquire_timeout, self.semapore.acquire())
+            .await
+            .map_err(|_| RunCommandError::SemaphoreAcquireError)?;
+
+        let permit = result.map_err(|_| RunCommandError::SemaphoreAcquireError)?;
+
+        Ok(permit)
+    }
+}
+
+#[async_trait]
+impl CommandsService for CommandsServiceImpl {
+    async fn run_command(&self, command_id: &str) -> Result<RunCommandResponse, RunCommandError> {
+        let command_info = *self
+            .id_to_command_info
+            .get(command_id)
+            .ok_or(RunCommandError::CommandNotFound)?;
+
+        let permit = self.acquire_semaphore().await?;
+
+        let command_start_time = Instant::now();
+        let command_result = Command::new(command_info.command())
+            .args(command_info.args())
+            .output()
+            .await;
+        let command_duration = command_start_time.elapsed();
+
+        drop(permit);
+
+        Ok(RunCommandResponse {
+            now: current_time_string(),
+            command_duration_ms: command_duration.as_millis(),
+            command_info: command_info,
+            command_output: match command_result {
+                Err(err) => {
+                    format!("error running command {}", err)
+                }
+                Ok(command_output) => {
+                    let mut combined_output = String::with_capacity(
+                        command_output.stderr.len() + command_output.stdout.len(),
+                    );
+                    combined_output.push_str(&String::from_utf8_lossy(&command_output.stderr));
+                    combined_output.push_str(&String::from_utf8_lossy(&command_output.stdout));
+                    combined_output
+                }
+            },
+        })
+    }
+}
+
+fn current_time_string() -> String {
+    Local::now().to_rfc3339_opts(SecondsFormat::Nanos, true)
+}
