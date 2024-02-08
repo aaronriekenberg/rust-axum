b38c89b66a281e6ef7606263a97896b2a923a6ad
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-02-07 21:11:49 -0600

    Add download_and_restart.sh.

---
+++ scripts/download_and_restart.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -x
+
+DONE=false
+
+PROJECT_PATH=$(realpath $(dirname $0)/..)
+echo "PROJECT_PATH = $PROJECT_PATH"
+
+cd $PROJECT_PATH
+
+while [ $DONE = "false" ] ; do
+
+  echo "begin loop $(date)"
+
+  git pull -v
+
+  RELEASE=$(git describe --abbrev=0 --tags)
+  echo "RELEASE=$RELEASE"
+
+  URL="https://github.com/aaronriekenberg/rust-axum/releases/download/${RELEASE}/rust-axum-$(arch)-unknown-linux-gnu.tar.gz"
+
+  wget $URL
+  WGET_RESULT=$?
+  if [ $WGET_RESULT -eq 0 ] ; then
+    DONE=true
+  else
+    echo "wget failure result $WGET_RESULT sleeping"
+    DONE=false
+    sleep 60
+  fi
+
+done
+
+systemctl --user stop rust-axum.service
+
+rm -fr target
+mkdir -p target/release
+cd target/release
+
+mv $PROJECT_PATH/rust-axum-$(arch)-unknown-linux-gnu.tar.gz .
+tar xvf rust-axum-$(arch)-unknown-linux-gnu.tar.gz
+
+#sudo setcap cap_net_bind_service=+ep ./rhs
+
+systemctl --user restart rust-axum.service
