3dc3ca98fb02cfe01c55fe338ebbdba01f9ba896
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-02-07 21:07:42 -0600

    Add create_release github workflows.

---
+++ scripts/create_release.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -x -e
+
+if [ $# -ne 1 ]; then
+  echo "usage: $0 <release version>"
+  exit 1
+fi
+
+PROJECT_PATH=$(realpath $(dirname $0)/..)
+echo "PROJECT_PATH = $PROJECT_PATH"
+
+RELEASE_VERSION=$1
+
+if [[ $RELEASE_VERSION != v* ]]; then
+  echo "RELEASE_VERSION $RELEASE_VERSION does not begin with 'v'"
+  exit 1
+fi
+
+RELEASE_VERSION_WITHOUT_V=$(echo $RELEASE_VERSION | sed -e 's/^v//g')
+
+echo "RELEASE_VERSION=$RELEASE_VERSION"
+echo "RELEASE_VERSION_WITHOUT_V=$RELEASE_VERSION_WITHOUT_V"
+
+cd $PROJECT_PATH
+
+toml set Cargo.toml package.version $RELEASE_VERSION_WITHOUT_V > Cargo.toml.tmp
+
+mv Cargo.toml.tmp Cargo.toml
+
+cargo build -v
+
+cargo test -v
+
+git add Cargo.toml Cargo.lock
+
+git commit -m "Version $RELEASE_VERSION_WITHOUT_V"
+
+git tag $RELEASE_VERSION
+
+git push -v
+git push -v --tags
