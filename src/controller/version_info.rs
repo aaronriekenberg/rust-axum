4d9c56fd2a8058224f700d731e3eca3c46415082
Aaron.Riekenberg <aaron.riekenberg@target.com>
2024-02-10 16:17:37 -0600

    Add use.

--- src/controller/version_info.rs
+++ src/controller/version_info.rs
@@ -1,5 +1,7 @@
 use axum::{body::Body, http::Request, response::IntoResponse, Json};
 
+use crate::service::version_service;
+
 pub async fn get_version_info(_request: Request<Body>) -> impl IntoResponse {
-    Json(crate::service::version_service::get_verison_info())
+    Json(version_service::get_verison_info())
 }
