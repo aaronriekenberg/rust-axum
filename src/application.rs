e291ed3dd7d9574b999a21485ac34897ebee0abe
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2024-02-25 10:03:24 -0600

    Add application::server.

--- src/application.rs
+++ src/application.rs
@@ -1,19 +1,8 @@
-use anyhow::Context;
+mod server;
 
-use axum::{
-    extract::{connect_info, ConnectInfo},
-    middleware::AddExtension,
-    Router,
-};
+use axum::Router;
 
-use hyper_util::{
-    rt::{TokioExecutor, TokioIo},
-    server,
-};
-
-use tokio::net::{UnixListener, UnixStream};
-
-use tower::{Service, ServiceBuilder};
+use tower::ServiceBuilder;
 
 use tower_http::{
     timeout::TimeoutLayer,
@@ -21,19 +10,9 @@ use tower_http::{
     ServiceBuilderExt,
 };
 
-use tracing::{debug, info, instrument, warn};
+use std::sync::Arc;
 
-use std::{convert::Infallible, path::PathBuf, sync::Arc};
-
-use crate::{
-    config::{self, ServerConfiguration},
-    controller,
-    service::{
-        self,
-        connection_service::{ConnectionGuard, ConnectionID, DynConnectionTrackerService},
-    },
-    utils::request::CounterRequestId,
-};
+use crate::{config, controller, service, utils};
 
 pub async fn run(config_file: String) -> anyhow::Result<()> {
     config::read_configuration(config_file).await?;
@@ -53,7 +32,7 @@ pub async fn run(config_file: String) -> anyhow::Result<()> {
         .layer(
             ServiceBuilder::new()
                 // make sure to set request ids before the request reaches `TraceLayer`
-                .set_x_request_id(CounterRequestId::default())
+                .set_x_request_id(utils::request::CounterRequestId::default())
                 // log requests and responses
                 .layer(
                     TraceLayer::new_for_http()
@@ -66,90 +45,5 @@ pub async fn run(config_file: String) -> anyhow::Result<()> {
                 .into_inner(),
         );
 
-    run_server(routes, server_configuration, connection_tracker_service).await
-}
-
-async fn create_listener(
-    server_configuration: &ServerConfiguration,
-) -> anyhow::Result<UnixListener> {
-    let path = PathBuf::from(&server_configuration.unix_socket_path);
-
-    let remove_result = tokio::fs::remove_file(&path).await;
-    debug!("remove_result = {:?}", remove_result);
-
-    let unix_listener = UnixListener::bind(&path).context("UnixListener::bind error")?;
-
-    info!("listening on uds path: {:?}", path);
-
-    Ok(unix_listener)
-}
-
-async fn run_server(
-    routes: Router,
-    server_configuration: &ServerConfiguration,
-    connection_tracker_service: DynConnectionTrackerService,
-) -> anyhow::Result<()> {
-    let listener = create_listener(server_configuration).await?;
-
-    let mut make_service = routes.into_make_service_with_connect_info::<ConnectionID>();
-
-    loop {
-        let connection_tracker_service_clone = Arc::clone(&connection_tracker_service);
-
-        let (socket, _remote_addr) = listener.accept().await.context("listener accept error")?;
-
-        let connection_guard = connection_tracker_service_clone.add_connection().await;
-
-        let tower_service = unwrap_infallible(make_service.call(&connection_guard.id).await);
-
-        tokio::spawn(handle_connection(connection_guard, socket, tower_service));
-    }
-}
-
-#[instrument(
-    name = "conn",
-    skip_all,
-    fields(
-        id = connection_guard.id.as_usize(),
-    )
-)]
-async fn handle_connection(
-    connection_guard: ConnectionGuard,
-    socket: UnixStream,
-    tower_service: AddExtension<Router, ConnectInfo<ConnectionID>>,
-) {
-    info!("begin handle_connection");
-
-    let socket = TokioIo::new(socket);
-
-    let hyper_service = hyper::service::service_fn(|request| {
-        connection_guard.increment_num_requests();
-        tower_service.clone().call(request)
-    });
-
-    if let Err(err) = server::conn::auto::Builder::new(TokioExecutor::new())
-        .serve_connection(socket, hyper_service)
-        .await
-    {
-        warn!("failed to serve connection: {err:#}");
-    }
-
-    info!(
-        "end handle_connection num requests = {}",
-        connection_guard.num_requests(),
-    );
-}
-
-fn unwrap_infallible<T>(result: Result<T, Infallible>) -> T {
-    match result {
-        Ok(value) => value,
-        Err(err) => match err {},
-    }
-}
-
-impl connect_info::Connected<&ConnectionID> for ConnectionID {
-    fn connect_info(id: &ConnectionID) -> Self {
-        debug!("in connect_info id = {id:?}");
-        *id
-    }
+    self::server::run(routes, server_configuration, connection_tracker_service).await
 }
---
+++ src/application/server.rs
@@ -0,0 +1,110 @@
+use anyhow::Context;
+
+use axum::{
+    extract::{connect_info, ConnectInfo},
+    middleware::AddExtension,
+    Router,
+};
+
+use hyper_util::{
+    rt::{TokioExecutor, TokioIo},
+    server,
+};
+
+use tokio::net::{UnixListener, UnixStream};
+
+use tower::Service;
+
+use tracing::{debug, info, instrument, warn};
+
+use std::{convert::Infallible, path::PathBuf, sync::Arc};
+
+use crate::{
+    config::ServerConfiguration,
+    service::connection_service::{ConnectionGuard, ConnectionID, DynConnectionTrackerService},
+};
+
+pub async fn run(
+    routes: Router,
+    server_configuration: &ServerConfiguration,
+    connection_tracker_service: DynConnectionTrackerService,
+) -> anyhow::Result<()> {
+    let listener = create_listener(server_configuration).await?;
+
+    let mut make_service = routes.into_make_service_with_connect_info::<ConnectionID>();
+
+    loop {
+        let connection_tracker_service_clone = Arc::clone(&connection_tracker_service);
+
+        let (socket, _remote_addr) = listener.accept().await.context("listener accept error")?;
+
+        let connection_guard = connection_tracker_service_clone.add_connection().await;
+
+        let tower_service = unwrap_infallible(make_service.call(&connection_guard.id).await);
+
+        tokio::spawn(handle_connection(connection_guard, socket, tower_service));
+    }
+}
+
+async fn create_listener(
+    server_configuration: &ServerConfiguration,
+) -> anyhow::Result<UnixListener> {
+    let path = PathBuf::from(&server_configuration.unix_socket_path);
+
+    let remove_result = tokio::fs::remove_file(&path).await;
+    debug!("remove_result = {:?}", remove_result);
+
+    let unix_listener = UnixListener::bind(&path).context("UnixListener::bind error")?;
+
+    info!("listening on uds path: {:?}", path);
+
+    Ok(unix_listener)
+}
+
+#[instrument(
+    name = "conn",
+    skip_all,
+    fields(
+        id = connection_guard.id.as_usize(),
+    )
+)]
+async fn handle_connection(
+    connection_guard: ConnectionGuard,
+    socket: UnixStream,
+    tower_service: AddExtension<Router, ConnectInfo<ConnectionID>>,
+) {
+    info!("begin handle_connection");
+
+    let socket = TokioIo::new(socket);
+
+    let hyper_service = hyper::service::service_fn(|request| {
+        connection_guard.increment_num_requests();
+        tower_service.clone().call(request)
+    });
+
+    if let Err(err) = server::conn::auto::Builder::new(TokioExecutor::new())
+        .serve_connection(socket, hyper_service)
+        .await
+    {
+        warn!("failed to serve connection: {err:#}");
+    }
+
+    info!(
+        "end handle_connection num requests = {}",
+        connection_guard.num_requests(),
+    );
+}
+
+fn unwrap_infallible<T>(result: Result<T, Infallible>) -> T {
+    match result {
+        Ok(value) => value,
+        Err(err) => match err {},
+    }
+}
+
+impl connect_info::Connected<&ConnectionID> for ConnectionID {
+    fn connect_info(id: &ConnectionID) -> Self {
+        debug!("in connect_info id = {id:?}");
+        *id
+    }
+}
